7670b18c584836e0f19f411e693dfdb50176a4f4
Chris Gramme <cgramme36@gmail.com>
2016-01-04 03:05:45 -0500

    ki

--- cube.js
+++ cube.js
@@ -6,9 +6,11 @@ $(window).load(function(){
 });
 
 $(document).ready(function(){
-	$('button').on('click',function(){
+		$('button').on('click',function(){
 		$('button').addClass('button-out');
 		$('.wrap div').addClass('start-dice');
-		setTimeout(function(){$('.wrap').addClass('roll-dice');},0);
+		$('.stage').addClass('fade-out');
+		setTimeout(function(){$('.wrap').addClass('roll-dice');$('.stage').removeClass('fade-out').addClass('start-dice');},1000);
+		setTimeout(function(){$('.wrap').removeClass('fade-out').addClass('start-dice');   $('.stage').removeClass('fade-out').addClass('start-dice');},7000);
 	});
 });

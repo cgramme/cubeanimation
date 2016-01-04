909f0cefab74aa2e3fb7e4d5d467a858f9822bc1
Chris Gramme <cgramme36@gmail.com>
2016-01-04 00:35:35 -0500

    j

--- cube.js
+++ cube.js
@@ -2,11 +2,12 @@
 $(window).load(function(){
 	$('.page-wrap').addClass('fade-slow');
 	setTimeout(function(){$('.stage').addClass('fade-fast');},500);
-	setTimeout(function(){$('button').addClass('fade-fast');},2000);
+	setTimeout(function(){$('button').addClass('button-in');},1000);
 });
 
 $(document).ready(function(){
 	$('button').on('click',function(){
-		$('button').fadeOut(500);
+		$('button').addClass('button-out');
+		$('.cubespinner div').addClass('start-dice');
 	});
 });

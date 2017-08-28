8c5632a89df21082875a5316577bb92c4c42469d
cgramme <cgramme36@gmail.com>
2017-08-28 03:47:41 -0400

    Update cube.js

--- cube.js
+++ cube.js
@@ -14,21 +14,21 @@ $(document).ready(function(){
 		clearInterval(textLoop);
 		$('.wrap div').removeClass('text-shadow');
 		$('button').addClass('button-out');
-		//$('.wrap div').addClass('start-dice');
+		$('.wrap div').addClass('start-dice');
 		//$('#stage div').css({"opacity":"0","transition":"1s"});
 		var number = 1 + Math.floor(Math.random() * 6);
 		setTimeout(function(){
 			$('.wrap').addClass('roll'+number);
 			//$('#stage div').css({"opacity":"1","transition":"2s"});},800);
-			//setTimeout(function(){$('.wrap div').addClass('roll-shadow');},3000);
-			//setTimeout(function(){$('button').unbind('mouseenter mouseleave :hover');
+			setTimeout(function(){$('.wrap div').addClass('roll-shadow');},3000);
+			setTimeout(function(){$('button').unbind('mouseenter mouseleave :hover');
 			//$('#stage div').css({"opacity":"0","transition":"2s"});
 		},5200);
 		setTimeout(function(){
 			$('.wrap').removeClass('roll'+number).addClass('cubespinner');
 			//$('#stage div').css({"opacity":"0.8","transition":"1s"});
 			$('button').removeClass('button-out').addClass('button-in');
-			//$('.wrap div').removeClass('roll-shadow');
+			$('.wrap div').removeClass('roll-shadow');
 		},7000);
 		//setTimeout(function(){randomHighlight();},9000);
 	});

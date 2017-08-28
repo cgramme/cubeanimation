da6658e52129675b0301b1c94d24d9035a0c1c75
cgramme <cgramme36@gmail.com>
2017-08-28 04:10:56 -0400

    Update cube.js

--- cube.js
+++ cube.js
@@ -15,22 +15,22 @@ $(document).ready(function(){
 		$('.wrap div').removeClass('text-shadow');
 		$('button').addClass('button-out');
 		$('.wrap div').addClass('start-dice');
-		$('#stage div').css({"opacity":"0","transition":"1s"});
+		$('#stage div').css({"opacity":"0"});
 		var number = 1 + Math.floor(Math.random() * 6);
 		setTimeout(function(){
 			$('.wrap').addClass('roll'+number);
-			$('#stage div').css({"opacity":"1","transition":"2s"});
+			$('#stage div').css({"opacity":"1"});
 		},800);
 		setTimeout(function(){
 			$('.wrap div').addClass('roll-shadow');
 		},3000);
 		setTimeout(function(){
 			$('button').unbind('mouseenter mouseleave :hover');
-			$('#stage div').css({"opacity":"0","transition":"2s"});
+			$('#stage div').css({"opacity":"0"});
 		},5200);
 		setTimeout(function(){
 			$('.wrap').removeClass('roll'+number).addClass('cubespinner');
-			$('#stage div').css({"opacity":"0.8","transition":"1s"});
+			$('#stage div').css({"opacity":"0.8"});
 			$('button').removeClass('button-out').addClass('button-in');
 			$('.wrap div').removeClass('roll-shadow');
 		},7000);
